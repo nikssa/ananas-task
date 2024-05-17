d0a3bb8b8719c02979a8cfc41dff0e453d853f30
Niksa Volarevic <nikssa@gmail.com>
2024-05-17 15:37:45 +0200

    Optimising the Posts.tsx component code

--- src/components/pages/Posts.tsx
+++ src/components/pages/Posts.tsx
@@ -1,35 +1,33 @@
-import { useEffect, useState } from 'react';
+import { useMemo, useState } from 'react';
 import Filter from '../common/Filter';
 
-import { LogProps } from '../../types';
+import { LogProps, PostProps, UserProps } from '../../types';
 
 import useAppContext from '../../hooks/useAppContext';
 import PostList from '../common/PostList';
 
 const Posts = ({ log }: LogProps) => {
-  log('Hello from', 'Posts component');
+  // log('Hello from', 'Posts component');
 
   const [keyword, setKeyword] = useState<string | null>(null);
-  const [filteredPosts, setFilteredPosts] = useState<any[]>([]);
 
   const { posts, users } = useAppContext();
 
-  useEffect(() => {
-    if (!!keyword) {
-      const filteredUsers = users.filter(
-        (user) =>
-          user.name.toLowerCase().includes(keyword.toLowerCase()) ||
-          user.username.toLowerCase().includes(keyword.toLowerCase())
-      );
-      const userIds = filteredUsers.map((user) => user.id);
-      const filteredPosts = posts.filter((post) => {
-        const filtered = userIds.filter((id) => post.userId === id);
-        return filtered.length > 0;
-      });
-      setFilteredPosts(filteredPosts);
-    } else {
-      setFilteredPosts(posts);
+  const filteredPosts = useMemo(() => {
+    if (!keyword) {
+      return posts;
     }
+
+    const lowerCaseKeyword = keyword.toLowerCase();
+    const userIds = users
+      .filter(
+        (user: UserProps) =>
+          user.name.toLowerCase().includes(lowerCaseKeyword) ||
+          user.username.toLowerCase().includes(lowerCaseKeyword)
+      )
+      .map((user: UserProps) => user.id);
+
+    return posts.filter((post: PostProps) => userIds.includes(post.userId));
   }, [keyword, users, posts]);
 
   return (
